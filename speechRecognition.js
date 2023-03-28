86e87cdeb4889c7eb9e0e47256bbc350b1a7ce04
ranj-ith-aa <ranjithaaa1368@gmail.com>
2023-03-28 23:56:49 +0530

    save

--- speechRecognition.js
+++ speechRecognition.js
@@ -55,15 +55,121 @@ if ("webkitSpeechRecognition" in window) {
   };
 
   document.querySelector("#save").onclick = () => {
+    const text = document.querySelector("#final").innerText;
+    const filename = "speech-to-text.txt";
+    const blob = new Blob([text], { type: "text/plain" });
+
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename);
+    } else {
+      const elem = window.document.createElement("a");
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = filename;
+      document.body.appendChild(elem);
+      elem.click();
+      document.body.removeChild(elem);
+    }
+  };
+    
+} else {
+  console.log("Speech Recognition Not Available");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*if ("webkitSpeechRecognition" in window) {
+  let speechRecognition = new webkitSpeechRecognition();
+  let final_transcript = "";
+
+  speechRecognition.continuous = true;
+  speechRecognition.interimResults = true;
+
+  speechRecognition.onstart = () => {
+    document.querySelector("#status").style.display = "block";
+  };
+  speechRecognition.onerror = () => {
+    document.querySelector("#status").style.display = "none";
+  };
+  speechRecognition.onend = () => {
+    document.querySelector("#status").style.display = "none";
+  };
+
+  speechRecognition.onresult = (event) => {
+    let interim_transcript = "";
+
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+
+    document.querySelector("#final").innerHTML = final_transcript;
+    document.querySelector("#interim").innerHTML = interim_transcript;
+  };
+
+  document.querySelector("#start").onclick = () => {
+    speechRecognition.lang = "kn-IN";
+    speechRecognition.start();
+  };
+
+  document.querySelector("#stop").onclick = () => {
+    speechRecognition.stop();
+  };
+
+  document.querySelector("#save").onclick = () => {
     let content = document.querySelector("#final").innerHTML;
     let savedContent = localStorage.getItem("savedContent") || "";
     let savedContentWithDate = savedContent + "\n\n" + new Date().toLocaleString() + "\n" + content;
     localStorage.setItem("savedContent", savedContentWithDate);
     document.querySelector("#savedText").innerHTML = "Saved content: " + content;
   };
+
 } else {
   console.log("Speech Recognition Not Available");
 }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
